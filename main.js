6f7ae581917d6fc68a7633222deafb2ae8370102
rsun <rsun@thoughtworks.com>
2019-09-10 14:19:58 +0800

    introduce https://github.com/greatbody/youdao-note-optimizer as built-in extention that fix bugs of youdao web version.

--- main.js
+++ main.js
@@ -8,15 +8,32 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
+
+  win.once('ready-to-show', () => {
+    win.show()
+  });
 
   win.webContents.on('new-window', function(e, url) {
     e.preventDefault();
     require('electron').shell.openExternal(url);
   });
 
+  win.webContents.on('dom-ready', () => {
+    win.webContents.executeJavaScript(`
+      const extention = document.createElement('script');
+      extention.src = 'https://cdn.jsdelivr.net/gh/greatbody/youdao-note-optimizer/dist/youdao-userscript.js';
+      extention.setAttribute('crossorigin', 'anonymous');
+      extention.setAttribute('type', 'text/javascript');
+      document.body.appendChild(extention);
+    `);
+  });
+
+  win.maximize();
+
   // and load the index.html of the app.
-  win.loadURL('https://note.youdao.com/web/')
+  win.loadURL('https://note.youdao.com/web/');
+
 }
 
 app.on('ready', createWindow)
